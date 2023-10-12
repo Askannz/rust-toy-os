8f2bf4512bb738594c8f96c27867e95eea258936
Robin Lange <robin.langenc@gmail.com>
2023-10-12 17:50:27 +1100

    WASM with bidirectional communication with host

--- kernel/src/wasmi_test.rs
+++ kernel/src/wasmi_test.rs
@@ -1,59 +1,59 @@
-use alloc::string::String;
-use core::mem;
+use alloc::vec;
 use crate::serial_println;
-use anyhow::{anyhow, Result};
-//use wasmi::*;
+use anyhow::Result;
 use wasmi::{Engine, Store, Func, Caller, Module, Linker};
 
 const WASM_CODE: &'static [u8] = include_bytes!("../../embedded_data/wasm_test.wasm");
 
+#[derive(Debug)]
+#[repr(C)]
+struct AppData {
+    n: u32
+}
+
+type HostState = ();
+
 pub fn wasmi_test() -> Result<()> {
-    // First step is to create the Wasm execution engine with some config.
-    // In this example we are using the default configuration.
+
+    serial_println!("Starting WASM test");
+
     let engine = Engine::default();
-
     let module = Module::new(&engine, WASM_CODE).unwrap();
-
-    // All Wasm objects operate within the context of a `Store`.
-    // Each `Store` has a type parameter to store host-specific data,
-    // which in this case we are using `42` for.
-    type HostState = u32;
-    let mut store = Store::new(&engine, 42);
-    let host_printouille = Func::wrap(&mut store, |mut caller: Caller<HostState>| {
-        serial_println!("Hello from WASM!")
+    let mut store: Store<HostState> = Store::new(&engine, ());
+    
+    let host_get_appdata = Func::wrap(&mut store, |caller: Caller<HostState>, addr: i32| {
+        let mem = caller.get_export("memory").unwrap().into_memory().unwrap();
+        let data = AppData { n: 1337 };
+        unsafe {
+            let ptr = &data as *const AppData as *const u8;
+            let buffer = core::slice::from_raw_parts(ptr, core::mem::size_of::<AppData>());
+            mem.write(caller, addr as usize, buffer).unwrap();
+        };
     });
 
-    // In order to create Wasm module instances and link their imports
-    // and exports we require a `Linker`.
+    let host_print_console = Func::wrap(&mut store, |caller: Caller<HostState>, addr: i32, len: i32| {
+        let mem = caller.get_export("memory").unwrap().into_memory().unwrap();
+        let len = len as usize;
+        let buffer = {
+            let mut buffer = vec![0u8; len];
+            mem.read(caller, addr as usize, buffer.as_mut()).unwrap();
+            buffer
+        };
+        let s = core::str::from_utf8(&buffer).unwrap();
+        serial_println!("Received from WASM: {}", s);
+    });
+
     let mut linker = <Linker<HostState>>::new(&engine);
-    // Instantiation of a Wasm module requires defining its imports and then
-    // afterwards we can fetch exports by name, as well as asserting the
-    // type signature of the function with `get_typed_func`.
-    //
-    // Also before using an instance created this way we need to start it.
-    linker.define("env", "printouille", host_printouille).unwrap();
+
+    linker.define("env", "host_get_appdata", host_get_appdata).unwrap();
+    linker.define("env", "host_print_console", host_print_console).unwrap();
     let instance = linker
         .instantiate(&mut store, &module).unwrap()
         .start(&mut store).unwrap();
     let hello = instance.get_typed_func::<(), i32>(&store, "hello").unwrap();
 
-    let memory = instance.get_memory(&mut store, "memory").unwrap();
-    // serial_println!("Before grow: {:?}", memory.data(&mut store).iter().max().unwrap());
-    // memory.grow(&mut store, wasmi::core::Pages::new(64).unwrap()).unwrap();
-    //serial_println!("After grow: {}", memory.data(&mut store).len());
-
-    serial_println!("Before calling: {}", store.data());
-
-    // And finally we can call the wasm!
+    serial_println!("Calling WASM");
     let res = hello.call(&mut store, ()).unwrap();
-
-    let res = res as usize;
-    let buffer = memory.data_mut(&mut store);
-    serial_println!("Wasm memory: {}", core::str::from_utf8(&buffer[res..res+4]).unwrap());
-
-    
-
-    serial_println!("After calling: {}", store.data());
 
     serial_println!("Result: {}", res);
 
--- wasm_apps/wasm-test/src/lib.rs
+++ wasm_apps/wasm-test/src/lib.rs
@@ -1,11 +1,63 @@
-const DATA: &'static str = "WASSUP";
+#![no_std]
+
+extern crate alloc;
+
+use core::panic::PanicInfo;
+use alloc::{string::String, format, vec::Vec};
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[derive(Debug)]
+#[repr(C)]
+struct AppData {
+    n: u32
+}
 
 extern "C" {
-    fn printouille();
+    fn host_print_console(addr: i32, len: i32);
+    fn host_get_appdata(addr: i32);
+}
+
+fn print_console(s: String) {
+    let buf = s.as_bytes();
+    let addr = buf.as_ptr() as i32;
+    let len = buf.len() as i32;
+    unsafe { host_print_console(addr, len) };
+}
+
+macro_rules! print {
+    ($($arg:tt)*) => {
+        print_console(format!($($arg)*))
+    };
+}
+
+macro_rules! println {
+    () => (print!("\n"));
+    ($fmt:expr) => (print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => (print!(concat!($fmt, "\n"), $($arg)*));
+}
+
+fn get_appdata() -> AppData {
+    let mut data: AppData = unsafe { 
+        core::mem::zeroed()
+    };
+    let addr = (&mut data) as *mut AppData as i32;
+    unsafe { host_get_appdata(addr) };
+    data
 }
 
 #[no_mangle]
 pub fn hello() -> i32 {
-    unsafe { printouille() };
-    DATA as *const str as *const u8 as i32
+
+    let app_data = get_appdata();
+    println!("Hello from WASM, {:?}", app_data);
+
+    42
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) ->  ! {
+    println!("{}", info);
+    loop {}
 }

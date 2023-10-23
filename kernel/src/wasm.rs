bc01d1e5133e850b5a729a238f761f8ec0f71c80
Robin Lange <robin.langenc@gmail.com>
2023-10-23 11:17:59 +1100

    WASM: app handles

--- kernel/src/wasm.rs
+++ kernel/src/wasm.rs
@@ -1,10 +1,10 @@
 use alloc::vec;
 use crate::serial_println;
-use wasmi::{Engine, Store, Func, Caller, Module, Linker, Config, TypedFunc};
+use wasmi::{Engine, Store, Func, Caller, Module, Linker, Config, TypedFunc, AsContextMut, Instance};
 
 #[derive(Debug)]
 #[repr(C)]
-struct AppData {
+struct AppHandle {
     n: u32
 }
 
@@ -23,17 +23,6 @@ impl WasmEngine {
 
         let module = Module::new(&self.engine, wasm_code).unwrap();
         let mut store: Store<()> = Store::new(&self.engine, ());
-
-        // TEST
-        let host_get_appdata = Func::wrap(&mut store, |caller: Caller<()>, addr: i32| {
-            let mem = caller.get_export("memory").unwrap().into_memory().unwrap();
-            let data = AppData { n: 1337 };
-            unsafe {
-                let ptr = &data as *const AppData as *const u8;
-                let buffer = core::slice::from_raw_parts(ptr, core::mem::size_of::<AppData>());
-                mem.write(caller, addr as usize, buffer).unwrap();
-            };
-        });
     
         let host_print_console = Func::wrap(&mut store, |caller: Caller<()>, addr: i32, len: i32| {
             let mem = caller.get_export("memory").unwrap().into_memory().unwrap();
@@ -48,33 +37,48 @@ impl WasmEngine {
         });
 
         let mut linker = <Linker<()>>::new(&self.engine);
-        linker.define("env", "host_get_appdata", host_get_appdata).unwrap();
         linker.define("env", "host_print_console", host_print_console).unwrap();
         let instance = linker
             .instantiate(&mut store, &module).unwrap()
             .start(&mut store).unwrap();
     
-        let wasm_init = instance.get_typed_func::<(), ()>(&store, "init").unwrap();
+        let wasm_init = instance.get_typed_func::<(), i32>(&store, "init").unwrap();
         let wasm_step = instance.get_typed_func::<(), ()>(&store, "step").unwrap();
 
-        wasm_init
+        let handle_addr = wasm_init
             .call(&mut store, ())
             .expect("Failed to initialize WASM app");
 
         WasmApp { 
             store,
+            instance,
             wasm_step,
+            handle_addr,
         }
     }
 }
 
 pub struct WasmApp {
     store: Store<()>,
+    instance: Instance,
     wasm_step: TypedFunc<(), ()>,
+    handle_addr: i32,
 }
 
 impl WasmApp {
     pub fn step(&mut self) {
+
+        let mem = self.instance.get_memory(&mut self.store, "memory").unwrap();
+        let ctx = self.store.as_context_mut();
+
+        let handle = AppHandle { n: 1337 };
+
+        unsafe {
+            let ptr = &handle as *const AppHandle as *const u8;
+            let buffer = core::slice::from_raw_parts(ptr, core::mem::size_of::<AppHandle>());
+            mem.write(ctx, self.handle_addr as usize, buffer).unwrap();
+        };
+
         self.wasm_step
             .call(&mut self.store, ())
             .expect("Failed to step WASM app");
--- wasm_apps/wasm-test/src/lib.rs
+++ wasm_apps/wasm-test/src/lib.rs
@@ -4,14 +4,13 @@ extern crate alloc;
 
 use core::panic::PanicInfo;
 use core::cell::OnceCell;
-use alloc::{string::String, format};
+use alloc::{string::String, format, boxed::Box};
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 extern "C" {
     fn host_print_console(addr: i32, len: i32);
-    fn host_get_appdata(addr: i32);
 }
 
 fn print_console(s: String) {
@@ -39,25 +38,10 @@ fn panic(info: &PanicInfo) ->  ! {
     loop {}
 }
 
-
-trait Application {
-    fn new() -> Self;
-    fn step(&mut self);
-}
-
 #[derive(Debug)]
 #[repr(C)]
-struct AppData {
+struct AppHandle {
     n: u32
-}
-
-fn get_appdata() -> AppData {
-    let mut data: AppData = unsafe { 
-        core::mem::zeroed()
-    };
-    let addr = (&mut data) as *mut AppData as i32;
-    unsafe { host_get_appdata(addr) };
-    data
 }
 
 
@@ -67,37 +51,49 @@ fn get_appdata() -> AppData {
 
 
 #[derive(Debug)]
-struct TestApp {
+struct AppState {
+    handle_ptr: *mut AppHandle,
     s: String,
     n: u64
 }
 
-static mut LOCAL_DATA: OnceCell<TestApp> = OnceCell::new();
-
-impl Application for TestApp {
-
-    fn new() -> Self {
-        TestApp { s: "Pouet".into(), n: 0 }
-    }
-
-    fn step(&mut self) {
-        println!("App state: {:?}", self);
-        self.n += 1;
-    }
-}
+static mut APP_STATE: OnceCell<AppState> = OnceCell::new();
 
 #[no_mangle]
-pub fn init() {
-    unsafe { 
-        let app = TestApp::new();
-        LOCAL_DATA.set(app).expect("Application was already initialized")
+pub fn init() -> i32 {
+
+    let handle: AppHandle = unsafe { core::mem::zeroed() };
+    let handle = Box::new(handle);
+
+    let handle_ptr = Box::leak(handle) as *mut AppHandle;
+    let handle_addr = handle_ptr  as i32;
+
+    let state = AppState {
+        handle_ptr,
+        s: "aaaa".into(),
+        n: 0,
     };
+
+    unsafe { 
+        APP_STATE
+            .set(state)
+            .expect("Application was already initialized");
+    };
+
+    handle_addr
 }
 
 #[no_mangle]
 pub fn step() {
-    unsafe { 
-        let app = LOCAL_DATA.get_mut().expect("Application is not initialized");
-        app.step();
-    }
+
+    let state = unsafe { 
+        APP_STATE
+            .get_mut()
+            .expect("Application is not initialized")
+    };
+
+    state.n += 1;
+    let handle = unsafe { core::ptr::read_volatile(state.handle_ptr) };
+
+    println!("{:?} {:?}", state, handle);
 }

7143f009fc3b50da9a315efa56eed5c851ffdaf0
Robin Lange <robin.langenc@gmail.com>
2023-11-06 15:27:44 +1100

    Safe-ish stdlib for WASM apps

---
+++ guestlib/src/lib.rs
@@ -0,0 +1,86 @@
+#![no_std]
+extern crate alloc;
+
+use core::panic::PanicInfo;
+use core::fmt::Debug;
+use core::mem::size_of;
+use alloc::{string::String, format, vec};
+use applib::{SystemState, Framebuffer};
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+extern "C" {
+    fn host_print_console(addr: i32, len: i32);
+    fn host_get_system_state(addr: i32);
+    fn host_set_framebuffer(addr: i32, w: i32, h: i32);
+}
+
+
+#[derive(Debug)]
+pub struct FramebufferHandle {
+    framebuffer_ptr: *mut u8,
+    w: usize,
+    h: usize,
+}
+
+pub fn print_console(s: String) {
+    let buf = s.as_bytes();
+    let addr = buf.as_ptr() as i32;
+    let len = buf.len() as i32;
+    unsafe { host_print_console(addr, len) };
+}
+
+pub fn get_system_state() -> SystemState {
+    let mut buf = [0u8; size_of::<SystemState>()];
+    let addr = buf.as_mut_ptr() as i32;
+    unsafe { 
+        host_get_system_state(addr);
+        core::mem::transmute(buf)
+    }
+}
+
+pub fn create_framebuffer(w: usize, h: usize) -> FramebufferHandle {
+    let ptr = vec![128u8; w*h*4].leak().as_mut_ptr();
+    unsafe { host_set_framebuffer(ptr as i32, w as i32, h as i32) };
+    FramebufferHandle {
+        framebuffer_ptr: ptr,
+        w,
+        h,
+    }
+}
+
+pub fn get_framebuffer<'a>(handle: &'a mut FramebufferHandle) -> Framebuffer<'a> {
+
+    let FramebufferHandle { framebuffer_ptr, w, h } = *handle;
+
+    let fb_data = unsafe {
+        core::slice::from_raw_parts_mut(framebuffer_ptr, w*h*4)
+    };
+
+    Framebuffer  {
+        data: fb_data,
+        w: w as i32,
+        h: h as i32,
+    }
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => {
+        $crate::print_console(format!($($arg)*))
+    };
+}
+
+#[macro_export]
+macro_rules! println {
+    () => (print!("\n"));
+    ($fmt:expr) => (print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => (print!(concat!($fmt, "\n"), $($arg)*));
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) ->  ! {
+    println!("{}", info);
+    loop {}
+}
--- wasm_apps/cube_3d/src/main.rs
+++ wasm_apps/cube_3d/src/main.rs
@@ -1,73 +1,16 @@
 #![no_std]
 #![no_main]
 
-extern crate alloc;
-
-use core::panic::PanicInfo;
 use core::cell::OnceCell;
-use core::mem::size_of;
-use alloc::{string::String, format};
-use alloc::vec;
-
-use applib::{SystemState, Framebuffer, Color};
+use applib::Color;
+use guestlib::FramebufferHandle;
 
 mod drawing;
-
 use drawing::draw_cube;
-
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-extern "C" {
-    fn host_print_console(addr: i32, len: i32);
-    fn host_get_system_state(addr: i32);
-    fn host_set_framebuffer(addr: i32, w: i32, h: i32);
-}
-
-fn print_console(s: String) {
-    let buf = s.as_bytes();
-    let addr = buf.as_ptr() as i32;
-    let len = buf.len() as i32;
-    unsafe { host_print_console(addr, len) };
-}
-
-fn get_system_state() -> SystemState {
-    let mut buf = [0u8; size_of::<SystemState>()];
-    let addr = buf.as_mut_ptr() as i32;
-    unsafe { 
-        host_get_system_state(addr);
-        core::mem::transmute(buf)
-    }
-}
-
-macro_rules! print {
-    ($($arg:tt)*) => {
-        print_console(format!($($arg)*))
-    };
-}
-
-macro_rules! println {
-    () => (print!("\n"));
-    ($fmt:expr) => (print!(concat!($fmt, "\n")));
-    ($fmt:expr, $($arg:tt)*) => (print!(concat!($fmt, "\n"), $($arg)*));
-}
-
-#[panic_handler]
-fn panic(info: &PanicInfo) ->  ! {
-    println!("{}", info);
-    loop {}
-}
-
-
-//
-//  END OF BOILERPLATE
-//
 
 #[derive(Debug)]
 struct AppState {
-    framebuffer_ptr: *mut u8,
-    w: usize,
-    h: usize,
+    fb_handle: FramebufferHandle
 }
 
 static mut APP_STATE: OnceCell<AppState> = OnceCell::new();
@@ -77,48 +20,19 @@ const H: usize = 200;
 
 #[no_mangle]
 pub fn init() -> () {
-
-    let framebuffer_ptr = unsafe {
-        let ptr = vec![128u8; W*H*4].leak().as_mut_ptr();
-        host_set_framebuffer(ptr as i32, W as i32, H as i32);
-        ptr
-    };
-
-    let state = AppState {
-        framebuffer_ptr,
-        w: W,
-        h: H,
-    };
-
-    unsafe { 
-        APP_STATE
-            .set(state)
-            .expect("Application was already initialized");
-    };
+    let fb_handle = guestlib::create_framebuffer(W, H);
+    let state = AppState { fb_handle };
+    unsafe { APP_STATE.set(state).expect("App already initialized"); }
 }
 
 #[no_mangle]
 pub fn step() {
 
-    let state = unsafe { 
-        APP_STATE
-            .get_mut()
-            .expect("Application is not initialized")
-    };
+    let state = unsafe { APP_STATE.get_mut().expect("App not initialized") };
 
-    let system_state = get_system_state();
+    let system_state = guestlib::get_system_state();
 
-    let mut framebuffer = unsafe {
-
-        let AppState { framebuffer_ptr, w, h } = *state;
-        let fb_data = core::slice::from_raw_parts_mut(framebuffer_ptr, w*h*4);
-
-        Framebuffer  {
-            data: fb_data,
-            w: w as i32,
-            h: h as i32,
-        }
-    };
+    let mut framebuffer = guestlib::get_framebuffer(&mut state.fb_handle);
 
     let pointer = system_state.pointer;
     let xf = (pointer.x as f32) / ((W - 1) as f32);
@@ -126,7 +40,5 @@ pub fn step() {
 
     let mut region = framebuffer.as_region();
     region.fill(&Color(0, 0, 0));
-    draw_cube(&mut region, xf, yf)
-
-    //println!("{:?} {:?}", state, system_state);
+    draw_cube(&mut region, xf, yf);
 }

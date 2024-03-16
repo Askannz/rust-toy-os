7b459cd2b49243381471a2fef87cb996bad0ea4e
Robin Lange <robin.langenc@gmail.com>
2024-03-16 17:35:59 +1100

    Have SystemClock return UNIX time

--- kernel/src/time.rs
+++ kernel/src/time.rs
@@ -1,11 +1,35 @@
 use uefi::prelude::RuntimeServices;
 
+use chrono::{NaiveDate, TimeZone, Utc, DateTime};
+
 pub struct SystemClock {
     period_s: f64,
+    epoch_offset: f64,
 }
 
 impl SystemClock {
     pub fn new(runtime_services: &RuntimeServices) -> Self {
+
+        let period_s = Self::estimate_cpu_period(runtime_services);
+        let secs_since_epoch = Self::get_epoch_time(runtime_services);
+
+        let n = unsafe { core::arch::x86_64::_rdtsc()};
+        let epoch_offset: f64 = secs_since_epoch - (n as f64 * period_s);
+
+        SystemClock { period_s, epoch_offset }
+    }
+
+    pub fn time(&self) -> f64 {  // in milliseconds
+        let n = unsafe { core::arch::x86_64::_rdtsc()};
+        1000f64 * (n as f64) * self.period_s + self.epoch_offset
+    }
+
+    pub fn spin_delay(&self, duration: f64) {
+        let t0 = self.time();
+        while self.time() - t0 < duration {}
+    }
+
+    fn estimate_cpu_period(runtime_services: &RuntimeServices) -> f64 {
 
         // Waiting for the "seconds" value to change
         let s1 = runtime_services.get_time().unwrap().second();
@@ -29,16 +53,32 @@ impl SystemClock {
             freq_ghz, n2 - n1
         );
 
-        SystemClock { period_s }
+        period_s
     }
 
-    pub fn time(&self) -> f64 {  // in milliseconds
-        let n = unsafe { core::arch::x86_64::_rdtsc()};
-        1000f64 * (n as f64) * self.period_s
-    }
+    fn get_epoch_time(runtime_services: &RuntimeServices) -> f64 {
 
-    pub fn spin_delay(&self, duration: f64) {
-        let t0 = self.time();
-        while self.time() - t0 < duration {}
+        let t_uefi = runtime_services.get_time().unwrap();
+
+        // We're just assuming the UEFI RTC clock returns UTC
+        let t_chrono = Utc.from_local_datetime(
+            &NaiveDate::from_ymd_opt(
+                t_uefi.year().into(), t_uefi.month().into(), t_uefi.day().into()
+            )
+            .expect("Invalid RTC clock")
+            .and_hms_opt(
+                t_uefi.hour().into(), t_uefi.minute().into(), t_uefi.second().into()
+            )
+            .expect("Invalid RTC clock")
+        ).unwrap();
+
+        let secs_since_epoch: u64 = 
+            (t_chrono - DateTime::UNIX_EPOCH)
+            .num_seconds().try_into()
+            .expect("Current time before UNIX epoch");
+
+        log::debug!("UNIX time: {}", secs_since_epoch);
+
+        secs_since_epoch as f64
     }
 }

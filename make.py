c803e0162ae71be2111a8e544057b8813766db93
Robin Lange <robin.langenc@gmail.com>
2024-10-12 14:00:14 +0200

    Added fix and fmt to make.py

--- make.py
+++ make.py
@@ -3,22 +3,53 @@
 import os
 import sys
 import shutil
+import argparse
 from pathlib import Path
 import subprocess
 
+
+WASM_APPS = [
+    "chronometer",
+    "cube_3d",
+    "terminal",
+    "web_browser",
+]
+
+CRATE_PATHS = [
+    "kernel/",
+    "applib/",
+    "guestlib/",
+    *[f"wasm_apps/{app}" for app in WASM_APPS]
+]
+
+
 def main():
+
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="cmd", required=True)
+    subparsers.add_parser("build")
+    subparsers.add_parser("run")
+    subparsers.add_parser("fmt")
+    subparsers.add_parser("fix")
+    args = parser.parse_args()
+
+    if args.cmd == "build":
+        _build()
+    elif args.cmd == "run":
+        _build()
+        _run()
+    elif args.cmd == "fmt":
+        _fmt()
+    elif args.cmd == "fix":
+        _fix()
+
+
+def _build():
 
     #
     # Building WASM apps
 
-    apps_list = [
-        "chronometer",
-        "cube_3d",
-        "terminal",
-        "web_browser",
-    ]
-
-    for app in apps_list:
+    for app in WASM_APPS:
 
         wasm_bin_path = _build_crate(
             crate_path=f"wasm_apps/{app}/",
@@ -32,44 +63,58 @@ def main():
     # Building kernel
 
     kernel_bin_path = _build_crate(
-        crate_path="kernel/",
+        crate_path="kernel/", 
         binary_name="kernel.efi",
         target="x86_64-unknown-uefi"
     )
 
     _copy_if_new(kernel_bin_path, Path("esp/efi/boot/") / "bootx64.efi")
 
+
+def _run():
+
     #
     # Running QEMU
 
-    qemu_args = " ".join([
+    qemu_args = " ".join(
+        [
+            "-enable-kvm",
+            "-m 4G",
+            "-rtc base=utc",
+            "-display gtk,zoom-to-fit=off",
 
-        "-enable-kvm",
-        "-m 4G",
-        "-rtc base=utc",
-        "-display gtk,zoom-to-fit=off",
+            # UEFI boot
+            "-drive if=pflash,format=raw,readonly=on,file=uefi_firmware/code.fd",
+            "-drive if=pflash,format=raw,readonly=on,file=uefi_firmware/vars.fd",
+            "-drive format=raw,file=fat:rw:esp",
 
-        # UEFI boot
-        "-drive if=pflash,format=raw,readonly=on,file=uefi_firmware/code.fd",
-        "-drive if=pflash,format=raw,readonly=on,file=uefi_firmware/vars.fd",
-        "-drive format=raw,file=fat:rw:esp",
+            # VirtIO peripherals
+            "-device virtio-keyboard",
+            "-device virtio-mouse",
+            "-device virtio-net-pci,netdev=network0 -netdev user,id=network0",
+            "-vga virtio",
 
-        # VirtIO peripherals
-        "-device virtio-keyboard",
-        "-device virtio-mouse",
-        "-device virtio-net-pci,netdev=network0 -netdev user,id=network0",
-        "-vga virtio",
-
-        # Debugging
-        "-monitor stdio",
-        "-serial file:log.txt",
-        #"-object filter-dump,id=f1,netdev=network0,file=dump.dat",
-    ])
+            # Debugging
+            "-monitor stdio",
+            "-serial file:log.txt",
+            # "-object filter-dump,id=f1,netdev=network0,file=dump.dat",
+        ]
+    )
 
     try:
         subprocess.check_call(f"qemu-system-x86_64 {qemu_args}", shell=True)
-    except KeyboardInterrupt:
+    except (KeyboardInterrupt, subprocess.CalledProcessError):
         sys.exit(1)
+
+
+def _fmt():
+    for crate_path in CRATE_PATHS:
+        subprocess.check_call("cargo fmt", shell=True, cwd=crate_path)
+
+
+def _fix():
+    for crate_path in CRATE_PATHS:
+        subprocess.check_call("cargo fix --allow-dirty", shell=True, cwd=crate_path)
 
 
 def _build_crate(
@@ -92,12 +137,8 @@ def _build_crate(
 
     print(f"Building {binary_path}")
     try:
-        subprocess.check_call(
-            f"cargo build --{mode}",
-            cwd=crate_path,
-            shell=True
-        )
-    except subprocess.CalledProcessError:
+        subprocess.check_call(f"cargo build --{mode}", cwd=crate_path, shell=True)
+    except (KeyboardInterrupt, subprocess.CalledProcessError):
         print("Build failed.")
         sys.exit(1)
 
@@ -113,27 +154,22 @@ def _check_source_changed(crate_path, binary_mtime):
             if (crate_path / "target") not in path.parents:
                 files_list.append(path)
 
-    changed_list = [
-        path for path in files_list
-        if path.lstat().st_mtime > binary_mtime
-    ]
+    changed_list = [path for path in files_list if path.lstat().st_mtime > binary_mtime]
 
     changed = len(changed_list) > 0
 
     if changed:
         print(f"Source changes in {crate_path}:")
-        print("\n".join(f' - {p}' for p in changed_list))
+        print("\n".join(f" - {p}" for p in changed_list))
 
     return changed
 
 
 def _copy_if_new(src, dst):
-    if (
-        not dst.exists() or
-        dst.lstat().st_mtime < src.lstat().st_mtime
-    ):
+    if not dst.exists() or dst.lstat().st_mtime < src.lstat().st_mtime:
         dst.parent.mkdir(parents=True, exist_ok=True)
         shutil.copy2(src, dst)
+
 
 if __name__ == "__main__":
     main()

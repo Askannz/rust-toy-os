792678b5a7d032776b81d789bf367c9260675ad3
Robin Lange <robin.langenc@gmail.com>
2024-12-26 15:35:04 +0100

    Implemented tile caching for buttons

--- applib/src/uitk/widgets/button.rs
+++ applib/src/uitk/widgets/button.rs
@@ -1,80 +1,109 @@
 use crate::drawing::primitives::draw_rect;
-use crate::drawing::text::{draw_str, Font, TextJustification, draw_line_in_rect};
-use crate::uitk::{UiContext};
-use crate::{Color, FbView, FbViewMut, Framebuffer, OwnedPixels, Rect};
+use crate::drawing::text::{draw_line_in_rect, draw_str, Font, FontFamily, TextJustification};
+use crate::uitk::{ContentId, UiContext};
+use crate::{Color, FbView, FbViewMut, Framebuffer, OwnedPixels, Rect, StyleSheet};
 use alloc::borrow::ToOwned;
 use alloc::string::String;
 
 impl<'a, F: FbViewMut> UiContext<'a, F> {
     pub fn button(&mut self, config: &ButtonConfig) -> bool {
-        #[derive(PartialEq)]
-        enum InteractionState {
-            Idle,
-            Hover,
-            Clicked,
-        }
 
         let UiContext {
-            fb, input_state, stylesheet, font_family, ..
+            fb, input_state, stylesheet, font_family, tile_cache, ..
         } = self;
 
-        let colorsheet = &stylesheet.colors;
         let ps = &input_state.pointer;
 
-        let interaction_state = match config.rect.check_contains_point(ps.x, ps.y) {
+        let state = match config.rect.check_contains_point(ps.x, ps.y) {
             true => match ps.left_click_trigger {
-                true => InteractionState::Clicked,
-                false => InteractionState::Hover,
+                true => ButtonState::Clicked,
+                false => ButtonState::Hover,
             },
-            false => InteractionState::Idle,
+            false => ButtonState::Idle,
         };
 
-        let button_color = match interaction_state {
-            InteractionState::Idle => colorsheet.element,
-            InteractionState::Hover => colorsheet.hover_overlay,
-            InteractionState::Clicked => colorsheet.selected_overlay,
-        };
+        let content_id = ContentId::from_hash((
+            state,
+            &config.rect,
+            &config.text,
+            // TODO: icon hash
+        ));
 
-        draw_rect(*fb, &config.rect, colorsheet.background, false);
+        let button_fb = tile_cache.fetch_or_create(content_id, self.time, || {
+            render_button(stylesheet, font_family, config, state)
+        });
 
-        let button_rect = {
-            let m = stylesheet.margin as i64;
-            let [x0, y0, x1, y1] = config.rect.as_xyxy();
-            Rect::from_xyxy([x0+m, y0+m, x1-m, y1-m])
-        };
+        let Rect { x0, y0, .. } = config.rect;
+        fb.copy_from_fb(button_fb, (x0, y0), false);
 
-        draw_rect(*fb, &button_rect, button_color, false);
-
-        let text_x0 = match &config.icon {
-            None => button_rect.x0,
-            Some(icon) => {
-                let (icon_w, icon_h) = icon.shape();
-                let m = i64::max(0, (button_rect.h - icon_h) as i64 / 2);
-
-                let icon_x0 = button_rect.x0 + m;
-                let icon_y0 = button_rect.y0 + m;
-
-                fb.copy_from_fb(*icon, (icon_x0, icon_y0), true);
-
-                icon_x0 + icon_w as i64
-            }
-        };
-
-        let text_rect = {
-            let [_x0, y0, x1, y1] = button_rect.as_xyxy();
-            Rect::from_xyxy([text_x0, y0, x1, y1])
-        };
-
-        let font = font_family.get_default();
-        draw_line_in_rect(
-            *fb, &config.text, &text_rect,
-            font,
-            colorsheet.text,
-            TextJustification::Left
-        );
-
-        interaction_state == InteractionState::Clicked
+        state == ButtonState::Clicked
     }
+}
+
+fn render_button(
+    stylesheet: &StyleSheet,
+    font_family: &FontFamily,
+    config: &ButtonConfig,
+    state: ButtonState
+) -> Framebuffer<OwnedPixels> {
+
+    let Rect { w, h, .. } = config.rect;
+    let mut button_fb = Framebuffer::new_owned(w, h);
+
+    let colorsheet = &stylesheet.colors;
+
+    let button_color = match state {
+        ButtonState::Idle => colorsheet.element,
+        ButtonState::Hover => colorsheet.hover_overlay,
+        ButtonState::Clicked => colorsheet.selected_overlay,
+    };
+
+    draw_rect(&mut button_fb, &config.rect, colorsheet.background, false);
+
+    let button_rect = {
+        let m = stylesheet.margin as i64;
+        let [x0, y0, x1, y1] = config.rect.zero_origin().as_xyxy();
+        Rect::from_xyxy([x0+m, y0+m, x1-m, y1-m])
+    };
+
+    draw_rect(&mut button_fb, &button_rect, button_color, false);
+
+    let text_x0 = match &config.icon {
+        None => button_rect.x0,
+        Some(icon) => {
+            let (icon_w, icon_h) = icon.shape();
+            let m = i64::max(0, (button_rect.h - icon_h) as i64 / 2);
+
+            let icon_x0 = m;
+            let icon_y0 = m;
+
+            button_fb.copy_from_fb(*icon, (icon_x0, icon_y0), true);
+
+            icon_x0 + icon_w as i64
+        }
+    };
+
+    let text_rect = {
+        let [_x0, y0, x1, y1] = button_rect.as_xyxy();
+        Rect::from_xyxy([text_x0, y0, x1, y1])
+    };
+
+    let font = font_family.get_default();
+    draw_line_in_rect(
+        &mut button_fb, &config.text, &text_rect,
+        font,
+        colorsheet.text,
+        TextJustification::Left
+    );
+
+    button_fb
+}
+
+#[derive(PartialEq, Hash, Clone, Copy)]
+enum ButtonState {
+    Idle,
+    Hover,
+    Clicked,
 }
 
 #[derive(Clone)]
